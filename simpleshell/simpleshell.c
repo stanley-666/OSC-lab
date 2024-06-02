96620e50b2252e1768d3807aa02b9379e80c67b6
Stanley-666 <stanley51018@gmail.com>
2024-06-03 00:47:44 +0800

    help command update

--- simpleshell/simpleshell.c
+++ simpleshell/simpleshell.c
@@ -1,47 +1,54 @@
 // simpleshell.c
-void shell(){
-	
-    while(1) {
-        char buffer[BUFFER_MAX_SIZE];
-        uart_send_string("# ");
-        read_command(buffer);
-        parse_command(buffer);
-    }
+#include "uart.h"
+#include "string.h"
+
+/*.global*/
+#define BUFFER_MAX_SIZE 5
+
+void read_command(char* buffer);
+void parse_command(char* buffer);
+
+void shell() {
+  while (1) {
+    char buffer[BUFFER_MAX_SIZE];
+
+    // Read command from target
+    read_command(buffer);
+
+    // Parse and execute command on host
+    parse_command(buffer);
+  }
 }
 
 void read_command(char* buffer) {
-    int index = 0;
-    while(1) {
-        buffer[index] = uart_get_char();
-        uart_send_char(buffer[index]);
-        //meet newline then we know this is the end of the string
-        if(buffer[index] == '\n') { 
-            buffer[index] = '\0';
-            buffer[index+1] = '\n';
-            break;
-        }
-        index++;
+  int index = 0;
+  // Print prompt
+  //uart_puts("[Waiting target's command]\n");
+  uart_puts("# ");
+  while (1) {
+    buffer[index] = uart_getc(); // get 對方之輸入
+    uart_send(buffer[index]); // send回去且-serial stdio會顯示在terminal
+    if (buffer[index] == '\n') {
+      buffer[index] = '\0';
+      buffer[index + 1] = '\n';
+      break;
     }
+    index++;
+  }
 }
 
 void parse_command(char* buffer) {
-    char* input_string = buffer;
-    char* parameter[5]; //5 is the available parameter length
-    int para_idx = 0;
-    int input_string_len = utils_strlen(input_string);
-    for(int i=0; i < input_string_len; i++){
-        if(*(input_string+i) == ' '){
-            *(input_string+i) = '\0';
-            //point to the start of next string
-            parameter[para_idx++] = (input_string+i+1);
-        }
-    }
-    
-    if(utils_string_compare(input_string,"help")) {
-       uart_send_string("help	:print this help menu\n");
-       uart_send_string("hello	:print Hello World!\n");
-       uart_send_string("info	:Get the hardware's information\n");
-    } else if (utils_string_compare(input_string,"hello")) {
-       uart_send_string("Hello World!\n");
-    }
+  char* input_string = buffer;
+ 
+  //uart_puts("[Parsing command from host]\n");
+  if (strcmp(input_string, "help") == 0) { // command
+    //uart_puts("[Responding from host]\n");
+    uart_puts("help  : list available commands\n");
+    uart_puts("hello : print Hello World!\n");
+    // Add more commands here
+  } else if (strcmp(input_string, "hello") == 0) {
+    uart_puts("Hello World!\n");
+  } else {
+    uart_puts("Unknown command\n");
+  }
 }
---
+++ simpleshell/string.h
@@ -0,0 +1,2 @@
+int strcmp(const char* str1, const char* str2);
+int strlen(const char* str);
